d224769850d3e4e5810c23cd2ab00c86686273a2
Esha Shetty <esha.dshetty@gmail.com>
2019-10-22 09:08:10 -0400

    constructor's, getters, setters

--- Rally/main.swift
+++ Rally/main.swift
@@ -10,16 +10,40 @@ import Foundation
 
 //declaring variables
 var option: String?
-let opt: Int?
+var opt1: Int?
+var opt2: String?
+var name: String?
+var maxSpeed: Double?
+var weight: Int?
+var fuel: Int?
+var category: String?
 
 //display option menu
 print("Welcome to Grand Prix")
 print("Select a option to proceed")
 print("1. Choose a car/motorcycle for racing")
 print("2. Add a vehicle to the existing list of Vehicles")
-//option = read();
-//opt = Int(readLine()!);
+opt1 = 1
+
+print("Select Vehicle Type")
+print("a. Car")
+print("b. Motorcycle")
+
+opt2 = "a"
+print("Enter Vehicle Name: ")
+print("Enter Vehicle Maximum Speed: ")
+let input:String?
+input = readLine()
+let int = Double(input!)!
+
+print("Enter Vehicle Weight: ")
+print("Enter Vehicle Fuel: ")
+print("Enter Vehicle Category: ")
+
+//let vehicle = Car(name:name!,maxSpeed: int!,weight: weight!,fuel:fuel!,category: category!)
+//let vehicleList = GrandPrix()
 //
-//print(opt)
-//let test = Moto(name:"Ferrarri",maxSpeed:35,weight: 3000,fuel: 45,sidecar: false)
-//test.displayVehicle()
+//vehicleList.add(vehicle: vehicle)
+
+//add vehicles in a loop
+//select cars to race
